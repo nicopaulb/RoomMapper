6e42d9e6549420942af3f05be20684400bb4f2ee
NBesnard <nico.besnard31@gmail.com>
2025-11-18 16:46:00 +0100

    rplidar: implement parser for received messages from RPLidar devices

--- Core/Inc/rplidar.h
+++ Core/Inc/rplidar.h
@@ -9,12 +9,48 @@
 #define INC_RPLIDAR_H_
 
 #include <stdint.h>
+#include "stm32f4xx_hal.h"
 
-void RPLIDAR_Init(void);
+typedef struct __attribute__((packed))
+{
+    uint8_t model_major : 4;
+    uint8_t model_sub : 4;
+    uint8_t fw_minor;
+    uint8_t fw_major;
+    uint8_t hardware;
+    uint8_t serial_nb[16];
+} rplidar_info_t;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t status;
+    uint16_t error_code;
+} rplidar_health_t;
+
+typedef struct __attribute__((packed))
+{
+    uint16_t tstandart;
+    uint16_t texpress;
+} rplidar_samplerate_t;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t start : 2;
+    uint8_t quality : 6;
+    uint8_t check : 1;
+    uint16_t angle : 15;
+    uint16_t distance;
+} rplidar_measurement_t;
+
+HAL_StatusTypeDef RPLIDAR_Init(UART_HandleTypeDef *huart);
 void RPLIDAR_StartScan(void);
 void RPLIDAR_Stop(void);
 void RPLIDAR_RequestHealth(void);
 void RPLIDAR_RequestDeviceInfo(void);
-void RPLIDAR_OnMeasurement(float angle_deg, float distance_mm);
+void RPLIDAR_OnSingleMeasurement(rplidar_measurement_t *measurement);
+void RPLIDAR_OnDeviceInfo(rplidar_info_t *info);
+void RPLIDAR_OnHealth(rplidar_health_t *health);
+void RPLIDAR_OnSampleRate(rplidar_samplerate_t *samplerate);
+void RPLIDAR_OnConfiguration(uint32_t type, uint8_t *payload, uint16_t payload_size);
 
 #endif /* INC_RPLIDAR_H_ */
--- Core/Src/rplidar.c
+++ Core/Src/rplidar.c
@@ -5,37 +5,78 @@
  *      Author: nicob
  */
 
-#include "rplidar.h"
-#include "string.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "stm32f4xx_hal.h"
+#include "rplidar.h"
 
 #define START_FLAG 0xA5
 
-// Request flag
-#define REQUEST_SCAN 0x20
-#define REQUEST_STOP 0x25
-#define REQUEST_RESET 0x40
-#define REQUEST_INFO 0x50
-#define REQUEST_HEALTH 0x52
-#define REQUEST_RATE 0x59
-#define REQUEST_SCAN_EXPR 0x82
-#define REQUEST_CONF 0x84
+#define REQ_SCAN 0x20
+#define REQ_STOP 0x25
+#define REQ_RESET 0x40
+#define REQ_INFO 0x50
+#define REQ_HEALTH 0x52
+#define REQ_RATE 0x59
+#define REQ_SCAN_EXPR 0x82
+#define REQ_CONF 0x84
 
-static uint8_t rpl_rx_buf[2048];
-static uint8_t rpl_response_buf[128];
+#define RESP_MODE_SINGLE 0x00
+#define RESP_MODE_MULTI 0x01
+#define RESP_FLAG 0x5A
+#define RESP_INFO 0x04
+#define RESP_HEALTH 0x06
+#define RESP_RATE 0x15
+#define RESP_CONF 0x20
+#define RESP_SCAN 0x81
+
+#define BUFFER_RX_SIZE 2048
+#define BUFFER_RESP_SIZE 128
+#define BUFFER_DESC_SIZE 7
+
+typedef enum parser_state
+{
+	PARSER_DESCRIPTOR,
+	PARSER_RESPONSE_SINGLE,
+	PARSER_RESPONSE_MULTI,
+	PARSER_ERROR
+} parser_state_t;
+
+typedef enum response_type
+{
+	RESPONSE_UNKNOWN,
+	RESPONSE_INFO,
+	RESPONSE_HEALTH,
+	RESPONSE_RATE,
+	RESPONSE_CONF,
+	RESPONSE_SCAN
+} response_type_t;
+
 static UART_HandleTypeDef *rpl_huart;
 
-static void _ParseResponse(uint8_t *data, uint16_t len);
-static void _ParseCabins(uint8_t *frame);
+static uint8_t rpl_rx_buf[BUFFER_RX_SIZE];
+static uint8_t rpl_resp_len = 0;
+static response_type_t rpl_resp_type = RESPONSE_UNKNOWN;
+static parser_state_t rpl_parser_state = PARSER_DESCRIPTOR;
+
+static void _ParseRX(uint8_t *data, uint16_t len);
+static parser_state_t _ParseDescriptor(uint8_t *buf);
+static response_type_t _ParseRspType(uint8_t type);
+static bool _ParseResponse(uint8_t *response, uint16_t size);
 static void _SendRequest(uint8_t cmd);
 
-HAL_StatusTypeDef RPLIDAR_Init(UART_HandleTypeDef *huart, rplidar_sample_cb_t cb, void *user_ctx) {
-	if (!huart) {
+HAL_StatusTypeDef RPLIDAR_Init(UART_HandleTypeDef *huart)
+{
+	if (!huart)
+	{
 		return HAL_ERROR;
 	}
+
 	rpl_huart = huart;
 
-	if (HAL_UARTEx_ReceiveToIdle_DMA(rpl_huart, rpl_rx_buf, sizeof(rpl_rx_buf)) != HAL_OK) {
+	if (HAL_UARTEx_ReceiveToIdle_DMA(rpl_huart, rpl_rx_buf, BUFFER_RX_SIZE) != HAL_OK)
+	{
 		return HAL_ERROR;
 	}
 
@@ -45,73 +86,223 @@ HAL_StatusTypeDef RPLIDAR_Init(UART_HandleTypeDef *huart, rplidar_sample_cb_t cb
 	return HAL_OK;
 }
 
-void RPLIDAR_StartScan(void) {
-	RPLIDAR_SendCommand(REQUEST_SCAN);
+void RPLIDAR_StartScan(void)
+{
+	_SendRequest(REQ_SCAN);
 }
 
-void RPLIDAR_Stop(void) {
-	RPLIDAR_SendCommand(REQUEST_STOP);
+void RPLIDAR_Stop(void)
+{
+	_SendRequest(REQ_STOP);
 }
 
-void RPLIDAR_RequestDeviceInfo(void) {
-	RPLIDAR_SendCommand(REQUEST_INFO);
+void RPLIDAR_RequestDeviceInfo(void)
+{
+	_SendRequest(REQ_INFO);
 }
 
-void RPLIDAR_RequestHealth(void) {
-	RPLIDAR_SendCommand(REQUEST_HEALTH);
+void RPLIDAR_RequestHealth(void)
+{
+	_SendRequest(REQ_HEALTH);
 }
 
-__weak void RPLIDAR_OnMeasurement(float angle_deg, float distance_mm) {
+__attribute__((weak)) void RPLIDAR_OnDeviceInfo(rplidar_info_t *info)
+{
 	return;
 }
 
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size) {
-	if (huart->Instance == rpl_huart->Instance) {
-		// Copy response and restart DMA
-		memcpy(rpl_response_buf, rplidar_rx_buf, size);
-		HAL_UARTEx_ReceiveToIdle_DMA(rpl_huart, rpl_rx_buf, sizeof(rpl_rx_buf));
+__attribute__((weak)) void RPLIDAR_OnHealth(rplidar_health_t *health)
+{
+	return;
+}
 
-		_ParseResponse(rpl_response_buf, size);
+__attribute__((weak)) void RPLIDAR_OnSampleRate(rplidar_samplerate_t *samplerate)
+{
+	return;
+}
+
+__attribute__((weak)) void RPLIDAR_OnConfiguration(uint32_t type, uint8_t *payload, uint16_t payload_size)
+{
+	return;
+}
+
+__attribute__((weak)) void RPLIDAR_OnSingleMeasurement(rplidar_measurement_t *measurement)
+{
+	return;
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t head)
+{
+	static uint16_t rpl_rx_tail = 0;
+	if (huart->Instance == rpl_huart->Instance)
+	{
+		if (head > rpl_rx_tail)
+		{
+			_ParseRX(&rpl_rx_buf[rpl_rx_tail], head - rpl_rx_tail);
+		}
+		else
+		{
+			// Buffer wrapped around
+			_ParseRX(&rpl_rx_buf[rpl_rx_tail], BUFFER_RX_SIZE - rpl_rx_tail);
+			_ParseRX(&rpl_rx_buf[0], head);
+		}
+		rpl_rx_tail = head;
 	}
 }
 
-static void _SendRequest(uint8_t cmd) {
-	uint8_t frame[2] = { START_FLAG, cmd };
-	HAL_UART_Transmit_IT(rplidar_huart, frame, sizeof(frame));
+static void _ParseRX(uint8_t *data, uint16_t len)
+{
+	for (uint16_t i = 0; i < len; i++)
+	{
+		switch (rpl_parser_state)
+		{
+		case PARSER_DESCRIPTOR:
+		{
+			static uint8_t rpl_desc_buf[BUFFER_DESC_SIZE];
+			static uint16_t rpl_desc_idx = 0;
+			rpl_desc_buf[rpl_desc_idx++] = data[i];
+			if (rpl_desc_idx == BUFFER_DESC_SIZE)
+			{
+				// Entire descriptor (7 bytes) received
+				rpl_parser_state = _ParseDescriptor(rpl_desc_buf);
+				rpl_desc_idx = 0;
+			}
+		}
+		break;
+		case PARSER_RESPONSE_SINGLE:
+		case PARSER_RESPONSE_MULTI:
+		{
+			static uint8_t rpl_resp_buf[BUFFER_RESP_SIZE];
+			static uint16_t rpl_resp_idx = 0;
+			rpl_resp_buf[rpl_resp_idx++] = data[i];
+			if (rpl_resp_idx == rpl_resp_len)
+			{
+				// Entire response received
+				if (_ParseResponse(rpl_resp_buf, rpl_resp_len))
+				{
+					// Parsing complete
+					if (rpl_parser_state == PARSER_RESPONSE_SINGLE)
+					{
+						// Single response was expected, ready for new descriptor
+						rpl_parser_state = PARSER_DESCRIPTOR;
+					}
+					else
+					{
+						// Multi response expected, ready for next responses
+					}
+				}
+				else
+				{
+					rpl_parser_state = PARSER_ERROR;
+				}
+				// Reset response buffer
+				rpl_resp_idx = 0;
+			}
+		}
+		break;
+
+			break;
+		case PARSER_ERROR:
+			return;
+		}
+	}
 }
 
-static void _ParseResponse(uint8_t *data, uint16_t len) {
-	if (len < 4)
-		return;
-
-	// Descriptor frame
-	if (data[0] == 0xA5 && data[1] == 0x5A) {
-		// TODO descriptor parsing
-		return;
+static bool _ParseResponse(uint8_t *response, uint16_t size)
+{
+	switch (rpl_resp_type)
+	{
+	case RESPONSE_INFO:
+		if (size == sizeof(rplidar_info_t))
+		{
+			rplidar_info_t *info = (rplidar_info_t *)response;
+			RPLIDAR_OnDeviceInfo(info);
+			return true;
+		}
+		break;
+	case RESPONSE_HEALTH:
+		if (size == sizeof(rplidar_health_t))
+		{
+			rplidar_health_t *health = (rplidar_health_t *)response;
+			RPLIDAR_OnHealth(health);
+			return true;
+		}
+		break;
+	case RESPONSE_RATE:
+		if (size == sizeof(rplidar_samplerate_t))
+		{
+			rplidar_samplerate_t *samplerate = (rplidar_samplerate_t *)response;
+			RPLIDAR_OnSampleRate(samplerate);
+			return true;
+		}
+		break;
+	case RESPONSE_CONF:
+		uint32_t type = response[0] << 24 | response[1] << 16 | response[2] << 8 | response[3];
+		RPLIDAR_OnConfiguration(type, response + 4, size - 4);
+		return true;
+	case RESPONSE_SCAN:
+		if (size == sizeof(rplidar_measurement_t))
+		{
+			rplidar_measurement_t *measurement = (rplidar_measurement_t *)response;
+			RPLIDAR_OnSingleMeasurement(measurement);
+			return true;
+		}
 	}
-
-	// Cabin frame
-	if (len == 96) {
-		_RPLIDAR_ParseCabins(data);
-		return;
-	}
-
-	// Ignore other packet sizes
+	return false;
 }
 
-static void _ParseCabins(uint8_t *frame) {
-	typedef struct __attribute__((packed)) {
-		uint16_t angle_q6;   // angle × 64
-		uint16_t dist_q2;    // distance × 4
-	} Cabin;
-
-	Cabin *cabins = (Cabin*) (frame + 4); // skip 4-byte header
-
-	for (int i = 0; i < 32; i++) {
-		float angle = cabins[i].angle_q6 / 64.0f;
-		float dist = cabins[i].dist_q2 / 4.0f;
-
-		RPLIDAR_OnMeasurement(angle, dist);
+static response_type_t _ParseRspType(uint8_t type)
+{
+	switch (type)
+	{
+	case RESP_INFO:
+		return RESPONSE_INFO;
+	case RESP_HEALTH:
+		return RESPONSE_HEALTH;
+	case RESP_RATE:
+		return RESPONSE_RATE;
+	case RESP_CONF:
+		return RESPONSE_CONF;
+	case RESP_SCAN:
+		return RESPONSE_SCAN;
+	default:
+		return RESPONSE_UNKNOWN;
 	}
 }
 
+static parser_state_t _ParseDescriptor(uint8_t *buf)
+{
+	typedef struct __attribute__((packed))
+	{
+		uint8_t flags[2];
+		uint32_t len : 30;
+		uint8_t mode : 2;
+		uint8_t type;
+	} descriptor_t;
+
+	descriptor_t *descriptor = (descriptor_t *)buf;
+
+	if (descriptor->flags[0] != START_FLAG || descriptor->flags[1] != RESP_FLAG)
+	{
+		return PARSER_ERROR;
+	}
+
+	rpl_resp_len = descriptor->len;
+	rpl_resp_type = _ParseRspType(descriptor->type);
+
+	switch (descriptor->mode)
+	{
+	case RESP_MODE_SINGLE:
+		return PARSER_RESPONSE_SINGLE;
+	case RESP_MODE_MULTI:
+		return PARSER_RESPONSE_MULTI;
+	default:
+		return PARSER_ERROR;
+	}
+}
+
+static void _SendRequest(uint8_t cmd)
+{
+	uint8_t packet[2] = {START_FLAG, cmd};
+	HAL_UART_Transmit_IT(rpl_huart, packet, sizeof(packet));
+}
